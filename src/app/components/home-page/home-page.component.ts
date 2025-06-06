bce5e27011907590e33664a1d9829be95ff8a997
Mariam Alsaeed <mariam.elgalady1@gmail.com>
2025-06-06 07:57:39 +0300

    Creating header component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RecentlyUpdatedComponent } from "../recently-updated/recently-updated.component";
-import { NewReleaseComponent } from "../new-release/new-release.component";
+import { RecentlyUpdatedComponent } from '../recently-updated/recently-updated.component';
+import { NewReleaseComponent } from '../new-release/new-release.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RecentlyUpdatedComponent, NewReleaseComponent],
+  imports: [RecentlyUpdatedComponent, NewReleaseComponent, HeaderComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}

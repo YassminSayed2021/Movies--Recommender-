e750ca0fd9715086c510582d9827f0a605cc06a5
Yassmin sayed <yassmin.sayed868@gmail.com>
2025-06-06 21:18:07 +0300

    homePageTask

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TMDB } from '../interfaces/tmdb';
+import { TmdbResponse } from '../interfaces/tmdb';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +14,27 @@ private apiKey = "9549fc06ebb41f9c445ad9c8dfdf0ce1"
   constructor(private http : HttpClient) { }
 
   getRecentlyUpdated() {
-    return this.http.get(`${this.baseUrl}/tv/on_the_air?api_key=${this.apiKey}&language=en-US&page=1`);
+    return this.http.get<{results:TMDB[]}>(`${this.baseUrl}/tv/on_the_air?api_key=${this.apiKey}&language=en-US&page=1`);
   }
 
 
 
-  getNewReleases(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/movie/now_playing?api_key=${this.apiKey}`);
+  getNewReleases(): Observable<TmdbResponse> {
+    return this.http.get<TmdbResponse>(`${this.baseUrl}/movie/now_playing?api_key=${this.apiKey}`);
   }
 
-  getNewSeries(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/tv/on_the_air?api_key=${this.apiKey}`);
+  getNewSeries(): Observable<TmdbResponse> {
+    return this.http.get<TmdbResponse>(`${this.baseUrl}/tv/on_the_air?api_key=${this.apiKey}`);
   }
+
+
+  getMovieRecommendations(id: number) {
+  return this.http.get<{results:TMDB[]}>(`${this.baseUrl}/movie/${id}/recommendations?api_key=${this.apiKey}`);
+}
+
+getSeriesRecommendations(id: number) {
+  return this.http.get<{results:TMDB[]}>(`${this.baseUrl}/tv/${id}/recommendations?api_key=${this.apiKey}`);
+}
 
 
 

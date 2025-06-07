6be994253f3c3ebc9e00993b04d87d712aac5e3d
Mariam Alsaeed <mariam.elgalady1@gmail.com>
2025-06-07 22:26:57 +0300

    Creating slider component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RecentlyUpdatedComponent } from '../recently-updated/recently-updated.component';
 import { NewReleaseComponent } from '../new-release/new-release.component';
 import { HeaderComponent } from '../header/header.component';
+import { SliderComponent } from '../slider/slider.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RecentlyUpdatedComponent, NewReleaseComponent, HeaderComponent],
+  imports: [
+    RecentlyUpdatedComponent,
+    NewReleaseComponent,
+    HeaderComponent,
+    SliderComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })

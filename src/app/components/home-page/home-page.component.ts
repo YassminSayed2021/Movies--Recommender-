cfe2ee3bc6454d6231cdef292a60fd4e338fd222
Yassmin sayed <yassmin.sayed868@gmail.com>
2025-06-08 04:21:11 +0300

    ai-chat

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -3,10 +3,11 @@ import { RecentlyUpdatedComponent } from "../recently-updated/recently-updated.c
 import { NewReleaseComponent } from "../new-release/new-release.component";
 import { RecommendedComponent } from "../recommended/recommended.component";
 import { AiChatComponent } from "../ai-chat/ai-chat.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [RecentlyUpdatedComponent, NewReleaseComponent, RecommendedComponent, AiChatComponent],
+  imports: [RecentlyUpdatedComponent, NewReleaseComponent, RecommendedComponent, AiChatComponent, HeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })

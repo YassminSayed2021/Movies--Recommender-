0393cbde0bae2cae2d17880f216b8f42b5e9aca4
Mariam Alsaeed <mariam.elgalady1@gmail.com>
2025-06-11 12:33:31 +0300

    Implemented routing

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,16 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import * as bootstrap from 'bootstrap';
-import { LoginComponent } from '../../login/login.component';
 import { AiChatComponent } from '../ai-chat/ai-chat.component';
-import { FooterComponent } from '../footer/footer.component';
-import { HeaderComponent } from '../header/header.component';
 import { NewReleaseComponent } from '../new-release/new-release.component';
 import { RecentlyUpdatedComponent } from '../recently-updated/recently-updated.component';
 import { RecommendedComponent } from '../recommended/recommended.component';
-import { SignupComponent } from '../signup/signup.component';
 import { SliderComponent } from '../slider/slider.component';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home-page',
@@ -20,64 +14,10 @@ import { AuthService } from '../../services/auth.service';
     NewReleaseComponent,
     RecommendedComponent,
     AiChatComponent,
-    HeaderComponent,
-    FooterComponent,
     SliderComponent,
-    SignupComponent,
-    LoginComponent,
     CommonModule,
   ],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent {
-  isLoggedIn: boolean = false;
-
-  constructor(private authService: AuthService) {
-    this.authService.isLoggedIn$.subscribe((status) => {
-      this.isLoggedIn = status;
-    });
-  }
-
-  openSignup() {
-    const el = document.getElementById('signupSidebar');
-    if (el) {
-      const instance = bootstrap.Offcanvas.getOrCreateInstance(el);
-      instance.show();
-    }
-  }
-
-  openLogin() {
-    const el = document.getElementById('loginSidebar');
-    if (el) {
-      const instance = bootstrap.Offcanvas.getOrCreateInstance(el);
-      instance.show();
-    }
-  }
-
-  switchFromSignupToLogin() {
-    const signup = bootstrap.Offcanvas.getInstance(
-      document.getElementById('signupSidebar')!
-    );
-    signup?.hide();
-    setTimeout(() => this.openLogin(), 300);
-  }
-
-  switchFromLoginToSignup() {
-    const login = bootstrap.Offcanvas.getInstance(
-      document.getElementById('loginSidebar')!
-    );
-    login?.hide();
-    setTimeout(() => this.openSignup(), 300);
-  }
-
-  onUserIconClick() {
-    if (!this.isLoggedIn) {
-      this.openSignup();
-    }
-  }
-
-  logout() {
-    this.authService.logout();
-  }
-}
+export class HomePageComponent {}
---
+++ src/app/components/layout/layout.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import * as bootstrap from 'bootstrap';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { SignupComponent } from '../signup/signup.component';
+import { LoginComponent } from '../login/login.component';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    RouterModule,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
+  ],
+  templateUrl: './layout.component.html',
+})
+export class LayoutComponent {
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {
+    this.authService.isLoggedIn$.subscribe((status) => {
+      this.isLoggedIn = status;
+    });
+  }
+
+  openSignup() {
+    const el = document.getElementById('signupSidebar');
+    if (el) {
+      const instance = bootstrap.Offcanvas.getOrCreateInstance(el);
+      instance.show();
+    }
+  }
+
+  openLogin() {
+    const el = document.getElementById('loginSidebar');
+    if (el) {
+      const instance = bootstrap.Offcanvas.getOrCreateInstance(el);
+      instance.show();
+    }
+  }
+
+  switchFromSignupToLogin() {
+    const signup = bootstrap.Offcanvas.getInstance(
+      document.getElementById('signupSidebar')!
+    );
+    signup?.hide();
+    setTimeout(() => this.openLogin(), 300);
+  }
+
+  switchFromLoginToSignup() {
+    const login = bootstrap.Offcanvas.getInstance(
+      document.getElementById('loginSidebar')!
+    );
+    login?.hide();
+    setTimeout(() => this.openSignup(), 300);
+  }
+
+  onUserIconClick() {
+    if (!this.isLoggedIn) {
+      this.openSignup();
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
